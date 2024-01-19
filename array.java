9b3314156601ec5e558cb4e5556dd644514be2fd
jiya <jiyahingrajiya@hmail.com>
2024-01-19 20:46:45 +0530

    anymsg

---
+++ array.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        //int[] marks={99,98,78,56,78,90,7,3,45,34,6,7,100};
+        // for(int i=0;i<14;i++){
+        //     if(i%2==0){
+        //         System.out.println(marks[i]);
+        //     }
+
+
+        /* 
+        int element= 90;
+        int i=0;
+        for(;i<14;i++){
+            if(element == marks[i]){
+                System.out.println(i);
+                break;
+            }
+        }
+        if(i==14){
+            System.out.println(-1);
+        }
+      */
+        
+
+
+      /*
+       in arrary for length no () needed
+        System.out.println(marks.length);
+       
+     
+       String[] names={"jiya","hasti","kritika","gandulog"};
+        System.out.println(names.length);
+       */
+
+
+    /*
+        int sum=0;
+
+       for(int i=0;i<marks.length;i++){
+        sum=sum+marks[i];
+       
+       }
+       System.out.println(sum);
+         
+        
+     */
+
+
+     /* how to take an input form array
+
+     
+      Scanner s = new Scanner(System.in);
+       int[] arr = new int[10];
+
+       for(int i=0;i<arr.length;i++){
+        arr[i]= s.nextInt();
+
+       }
+       for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+       }
+       
+      */
+       
+
+    }
+}
+
---
+++ strings.java
@@ -0,0 +1,16 @@
+public class strings {
+    public static void main(String[] args) {
+        String s = "helloo how are you";
+        System.out.println(s.length());
+        String s1 = s.toUpperCase();
+        String s2 = s.toLowerCase();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s.indexOf("how"));
+        String name = "jiya's";
+        String Surname ="hingrajiya";
+        System.out.println(name+" "+Surname);
+        String quote = "demo is \\going on\"jiya\" and \"kritika\"";
+        System.out.println(quote);
+    }
+}
